4e6aae750f54dc46dda8f40be7c5e61dc5878051
Radek <riw@hdsa.pl>
2023-11-15 11:56:23 +0100

    HelloWorldTestProject - creation

---
+++ HelloWorldApp/HelloWorldAppTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace HelloWorldAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true, "Should be true");
+        }
+    }
+}
